b9000f6eeb187592cecfb2cc7b40ee8cc0749654
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-03-02 16:19:57 -0500

    Try to exclude bad UDP tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,11 @@
+# We are going to disable some tests on qemu based platforms
+
+if [ ${target_platform} == linux-aarch64 ]; then
+   sed -i '/get_currentexe/d' ./test/test-list.h
+   sed -i '/udp_multicast_interface/d' ./test/test-list.h
+   sed -i '/udp_no_autobind/d' ./test/test-list.h
+fi
+
 # LIBTOOLIZE setting is required to workaround missing glibtoolize on OS X:
 # https://github.com/joyent/libuv/issues/1200
 LIBTOOLIZE=libtoolize sh ./autogen.sh
