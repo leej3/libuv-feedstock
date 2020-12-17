ecbf96cee08dd5bf5305db5c6a20abc8267776f4
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-12-17 17:39:56 +0100

    Release build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 mkdir build
 cd build
 
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release ..
 nmake install
